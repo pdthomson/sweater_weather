86d3e493fc31807742b2d3a0676ba0e1874e74e4
Parker Thomson <pdthomson1239@gmail.com>
2022-09-26 13:39:24 -0400

    sad path testing included passing

--- spec/api/v1/requests/book_search_request_spec.rb
+++ spec/api/v1/requests/book_search_request_spec.rb
@@ -5,8 +5,6 @@ RSpec.describe 'book/forecast for city search' do
   it 'returns json', :vcr do 
     get '/api/v1/book-search?location=seattle,wa&quantity=5'
 
-    expect(response).to be_successful
-    expect(response).to have_http_status(200)
     book_response = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
     expect(response.status).to eq(200)
@@ -14,14 +12,14 @@ RSpec.describe 'book/forecast for city search' do
     expect(book_response).to have_key(:data)
     expect(book_response[:data]).to have_key(:id)
     expect(book_response[:data]).to have_key(:type)
-    expect(book_response[:data][:type]).to eq("books")
     expect(book_response[:data]).to have_key(:attributes)
+    expect(book_response[:data][:type]).to eq("books")
     expect(book_response[:data][:attributes]).to have_key(:destination)
     expect(book_response[:data][:attributes]).to have_key(:forecast)
-    expect(book_response[:data][:attributes][:forecast]).to have_key(:summary)
-    expect(book_response[:data][:attributes][:forecast]).to have_key(:temperature)
     expect(book_response[:data][:attributes]).to have_key(:total_books_found)
     expect(book_response[:data][:attributes]).to have_key(:books)
+    expect(book_response[:data][:attributes][:forecast]).to have_key(:summary)
+    expect(book_response[:data][:attributes][:forecast]).to have_key(:temperature)
     book_response[:data][:attributes][:books].each do |book|
       expect(book).to have_key(:isbn)
       expect(book).to have_key(:title)
@@ -34,5 +32,25 @@ RSpec.describe 'book/forecast for city search' do
     expect(book_response[:data][:attributes]).to_not have_key(:last_modified)
   end
 
+  it 'returns 404 if params are invalid', :vcr do
+    params = { quantity: 5 }
 
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+    get '/api/v1/book-search', headers: headers, params: params
+
+    expect(response.status).to eq(404)
+  end
+
+  it "returns error message if quantity is not valid", :vcr do
+    params = { location: "denver,co", quantity: 50000000 }
+
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+    get '/api/v1/book-search', headers: headers, params: params
+    book_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(200)
+    expect(book_response).to have_key(:data)
+    expect(book_response[:data]).to have_key(:message)
+    expect(book_response[:data][:message]).to eq("Invalid Quantity")
+  end
 end
