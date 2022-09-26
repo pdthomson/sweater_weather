1e54133f2954934640710de52a13de7c12778ce0
Parker Thomson <pdthomson1239@gmail.com>
2022-09-26 01:59:12 -0400

    exception handler updated for sad path no location

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,8 +3,17 @@ module ExceptionHandler
   extend ActiveSupport::Concern 
 
   included do 
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      json_response({ message: e.message}, 400)
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      json_response({ message: e.message}, 400)
+    end
+
+    rescue_from ActionController::ParameterMissing do |e|
+      json_response({ message: e.message}, 400)
+    end
 
   end
-
-
 end
